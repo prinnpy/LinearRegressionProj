3bb3d034c27baf89ad25e3a3d678f6bd328f26a1
Prinn Prinyanut <Prinn@Prinns-MacBook-Pro.local>
2018-04-29 22:22:46 -0600

    create predictions

--- linearRegression.py
+++ linearRegression.py
@@ -13,6 +13,21 @@ customers = pd.read_csv('Ecommerce Customers')
 sns.set_palette("GnBu_d")
 sns.set_style('whitegrid')
 
-sns.jointplot(x='Time on Website', y='Yearly Amount Spent',kind='hex', data=customers)
+# sns.jointplot(x='Time on Website', y='Yearly Amount Spent',kind='hex', data=customers)
+
+y = customers['Yearly Amount Spent']
+X = customers[['Avg. Session Length', 'Time on App','Time on Website', 'Length of Membership']]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+lm = LinearRegression()
+
+lm.fit(X_train,y_train)
+
+predictions = lm.predict(X_test)
+
+plt.scatter(y_test,predictions)
+plt.xlabel('Y Test')
+plt.ylabel('Predicted Y')
 
 plt.show()
